0abf4c8871449b3966b7f7fd76769cf7f2877ef6
Zeurkeur <55217332+Zeurkeur@users.noreply.github.com>
2019-12-14 23:23:10 -0500

    Update readmemd.js

--- final-master/readmemd.js
+++ final-master/readmemd.js
@@ -1,2 +1,22 @@
-//Installed a package called ejs and json, node & nodemon,  and express
-//
+
+//Installed a package called ejs and json, node & nodemon, and express
+//For reference, in order for everything to work, the following items need to be installed in order for the entire app to work:
+//*Node / node.js / EJS / json / Express
+All code has been troubleshot and should be ready to go
+
+git clone: https://github.com/Zeurkeur/finalproject.git
+
+All images have also been provided within the github repository as well.
+
+*'/' is where the root directory should be.
+*127.0.0.1:3000 port is where it should start.
+*To initialize the project, change the cd in CMD to cd ...\final-master, then run a node myserver.js. Exepected response:
+"Server is running on port 3000"
+
+
+'Search' --> where the spacebar can be found at the top of the page where
+input can be placed where the list will automatically filter out what classes
+, professors, etc are available within the database.
+
+The searchbar down below that is a dropdown will show all the classes
+in a more traditional drop-down option style.
